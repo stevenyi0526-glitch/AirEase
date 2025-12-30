f350610241a1f3aaa72020c441694b8a486c4416
stevyee <s3b009@gmail.com>
2025-12-30 14:08:47 +0800

    Initial Commit

---
+++ AirEase/AirEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirEaseApp.swift
+//  AirEase
+//
+//  Created by Steven Yi on 30/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct AirEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirEaseTests/AirEaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirEaseTests.swift
+//  AirEaseTests
+//
+//  Created by Steven Yi on 30/12/2025.
+//
+
+import Testing
+@testable import AirEase
+
+struct AirEaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
